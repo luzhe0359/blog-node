b3c6b6155cb636c9cd893644163f4f8451ccf0a9
luzhe0359 <luzhe0359@163.com>
2022-07-12 21:52:20 +0800

    secret 隐藏加密文件

---
+++ config/secret.js
@@ -0,0 +1,10 @@
+/**
+ * 全局配置
+ */
+module.exports = {
+    CRYPTO_KEY: "example*********", // 16位加密密钥 
+    CRYPTO_IV: "example*********", // 16位偏移量, 可以与秘钥相同
+    ACCESS_KEY_ID: "exampleId**", // 阿里云OSS accessKeyId
+    ACCESS_KEY_SECRET: "exampleSecret**", // 阿里云OSS accessKeySecret
+    NODEMAILER_AUTH_PASS: "examplePass**", // 163邮箱生成的密码
+};
